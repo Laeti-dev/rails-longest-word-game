d2216a036d89a17c778a0663003d6cae513d7a7c
Laetitia Taddei <laetdi@gmail.com>
2022-08-27 10:13:41 +0200

    shuffle the letters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,19 @@
 class GamesController < ApplicationController
   def new
-    @letters = ('a'..'z').to_a.sample(10)
+    consonants = %w[B C D F G H J K L M N P Q R S T V W X Y Z]
+    vowels = %w[A E I O U]
+    @letters = []
+
+    6.times do
+      @letters << consonants[rand(21)]
+    end
+
+    4.times do
+      @letters << vowels[rand(5)]
+    end
   end
 
   def score
-    raise
+
   end
 end
