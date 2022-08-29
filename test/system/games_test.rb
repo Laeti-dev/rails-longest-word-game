c39b14cfa64425e9c605e34cf00dee6ec1850756
Laetitia Taddei <laetdi@gmail.com>
2022-08-29 09:46:13 +0200

    testing the non english word

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -29,12 +29,18 @@ class GamesTest < ApplicationSystemTestCase
     assert_text "Sorry, PLOUF can't be build out of #{letters.join(", ")}"
   end
 
-  # test "fill the form with a one-letter consonant word, click play, and get a message that the word is not a valid English word" do
-  #   visit new_url
-  #   fill_in 'word', with: 't'
-  #   click_on 'Play!'
+  test "fill the form with a one-letter consonant word, click play, and get a message that the word is not a valid English word" do
+    visit new_url
+    letters = []
+    all('div.letter-box').each do |box|
+      letters << box.text
+    end
+    letter = letters.sample
+    letter_up = letter.upcase
+    fill_in 'word', with: letter
+    click_on 'Play!'
 
-  #   assert_current_path score_path
-  #   assert_text "Sorry, T is not an english word"
-  # end
+    assert_current_path score_path
+    assert_text "Sorry, #{letter} is not an English word"
+  end
 end
